09f53026d81d9fd0fc5b1e8e77ac4f9c710860be
jacobhsu <jacob.hsu.tw@gmail.com>
2020-12-24 17:49:39 +0800

    react-iframe-comm

--- iframe-backend-app/src/Home.js
+++ iframe-backend-app/src/Home.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
+import IframeComm from "react-iframe-comm";
 
 function App() {
+  const mainUrl = "http://localhost:3003/" // npm start frontend first
+  const height = document.documentElement.clientHeight
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <IframeComm
+      attributes={{
+        id: "iframe",
+        src: mainUrl,
+        position: 'fixed',
+        width:'100%',
+        top: 'auto',
+        height: height,
+      }}
+      postMessageData={"test99 hello iframe"}
+      handleReady={()=>{
+        console.log("test99 onReady");
+      }}
+      />
     </div>
   );
 }
